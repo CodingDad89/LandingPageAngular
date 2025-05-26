618d9c697c5afd49fbc76d8a72d681d5fa6dfbfa
CodingDad89 <lutz.amgarten@web.de>
2025-05-26 11:15:53 +0200

    Add first component

--- sakura/src/app/LandingPage/LandingPage.component.ts
+++ sakura/src/app/LandingPage/LandingPage.component.ts
@@ -1,6 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+    selector: 'app-landing-page',
+    standalone: true,
+    imports: [CommonModule],
+    template: `
+    <section>
+        <h1>SAKURA RAMEN</h1>
+        <h2>BEST RAMEN IN TOWN</h2>
+    </section>
+        `,
+    styleUrls: ['./LandingPage.component.scss']    
 
 })
 
