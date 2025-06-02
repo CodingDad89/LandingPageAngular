7a7334fcdabd9dc5e2cf789c50710252f0f3195f
CodingDad89 <lutz.amgarten@web.de>
2025-06-02 12:04:10 +0200

    new sections

--- sakura/src/app/app.component.ts
+++ sakura/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainContentComponent } from "./main-content/main-content.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],  
+  imports: [RouterOutlet, CommonModule, MainContentComponent, FooterComponent, ImprintComponent],  
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- sakura/src/app/main-content/main-content.component.ts
+++ sakura/src/app/main-content/main-content.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './LandingPage/LandingPage.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [CommonModule, LandingPageComponent, OurMenuComponent, HowToOrderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
